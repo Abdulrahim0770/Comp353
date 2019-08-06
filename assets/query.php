b1567a61cd8313580157dbf9692986c5b7795b3b
Neeham Khalid <Neehamk@gmail.com>
2019-08-06 14:57:36 -0400

    New SQL query implemented.

--- assets/query.php
+++ assets/query.php
@@ -32,11 +32,11 @@
    $STH = $conn->query("SELECT CampusName AS 'Campus Name' FROM Campus");
 ?>
 <div class="panel-body">
-   <div class="modal fade" id="listOfCampus" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
          <div class="modal-content">
             <div class="modal-header">
-               <h5 class="modal-title" id="exampleModalCenterTitle">List of Campus</h5>
+               <h5 class="modal-title">List of Campus</h5>
                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                <span aria-hidden="true">&times;</span>
                </button>
@@ -72,7 +72,7 @@
 </div>
 <script>
    $(function() {
-     $("#listOfCampus").modal();
+     $("#myModal").modal();
    });
 </script>
 <?php
@@ -83,11 +83,11 @@
      $STH = $conn->query("SELECT CampusID, CampusName FROM Campus");
 ?>
 <div class="panel-body">
-   <div class="modal fade" id="listOfCampus" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
          <div class="modal-content">
             <div class="modal-header">
-               <h5 class="modal-title" id="exampleModalCenterTitle">List of Campus</h5>
+               <h5 class="modal-title">List of Campus</h5>
                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                <span aria-hidden="true">&times;</span>
                </button>
@@ -95,7 +95,7 @@
             <div class="modal-body">
                <form  method="post">
                   <label for="sel1">Choose a Campus</label>
-                  <select class="form-control" name="CampID">
+                  <select class="form-control" name="sendingCampusID">
 <?php
                      while($row = $STH->fetch()) {
                        echo "<option name='display' value='".$row['CampusID']."'>".$row['CampusName']."</option>";
@@ -103,7 +103,7 @@
 ?>
                   </select>
                   <div class="modal-footer">
-                     <button type="submit" class="btn btn-primary" name="getBuildingNames">Submit</button>
+                     <button type="submit" class="btn btn-primary" name="ButtonOne">Submit</button>
                   </div>
                </form>
             </div>
@@ -113,21 +113,21 @@
 </div>
 <script>
    $(function() {
-   $("#listOfCampus").modal();
+   $("#myModal").modal();
    });
 </script>
 <?php
    }
 
-   if(isset($_POST['getBuildingNames'])) {
-     $STH = $conn->query("SELECT Building.BuildingName AS 'Building Name' FROM Building WHERE Building.CampusID='".$_POST['CampID']."' ");
+   if(isset($_POST['ButtonOne'])) {
+     $STH = $conn->query("SELECT Building.BuildingName AS 'Building Name' FROM Building WHERE Building.CampusID='".$_POST['sendingCampusID']."' ");
 ?>
 <div class="panel-body">
-   <div class="modal fade" id="listOfCampus" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
          <div class="modal-content">
             <div class="modal-header">
-               <h5 class="modal-title" id="exampleModalCenterTitle">List of Building </h5>
+               <h5 class="modal-title">List of Building </h5>
                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                <span aria-hidden="true">&times;</span>
                </button>
@@ -163,7 +163,7 @@
 </div>
 <script>
    $(function() {
-     $("#listOfCampus").modal();
+     $("#myModal").modal();
    });
 </script>
 <?php
@@ -174,11 +174,11 @@
      $STH = $conn->query("SELECT BuildingID, BuildingName FROM Building");
 ?>
 <div class="panel-body">
-   <div class="modal fade" id="listOfCampus" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
          <div class="modal-content">
             <div class="modal-header">
-               <h5 class="modal-title" id="exampleModalCenterTitle">List of Building</h5>
+               <h5 class="modal-title">List of Building</h5>
                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                <span aria-hidden="true">&times;</span>
                </button>
@@ -186,15 +186,15 @@
             <div class="modal-body">
                <form  method="post">
                   <label for="sel1">Choose a Building</label>
-                  <select class="form-control" name="GetBuildDetails">
+                  <select class="form-control" name="sendingBuildingID">
 <?php
                      while($row = $STH->fetch()) {
-                       echo "<option name='display' value='".$row['BuildingName']."'>".$row['BuildingName']."</option>";
+                       echo "<option name='display' value='".$row['BuildingID']."'>".$row['BuildingName']."</option>";
                      }
 ?>
                   </select>
                   <div class="modal-footer">
-                     <button type="submit" class="btn btn-primary" name="GetBuilID">Submit</button>
+                     <button type="submit" class="btn btn-primary" name="ButtonTwo">Submit</button>
                   </div>
                </form>
             </div>
@@ -204,24 +204,24 @@
 </div>
 <script>
    $(function() {
-   $("#listOfCampus").modal();
+   $("#myModal").modal();
    });
 </script>
 <?php
    }
 
-   if(isset($_POST['GetBuilID'])) {
-   $STH = $conn->query("SELECT DISTINCT BuildingName, BuildingAddress, totalFloors FROM Building WHERE BuildingName = '".$_POST['GetBuildDetails']."'");
+   if(isset($_POST['ButtonTwo'])) {
+   $STH = $conn->query("SELECT DISTINCT BuildingName, BuildingAddress, totalFloors FROM Building WHERE BuildingID = '".$_POST['sendingBuildingID']."'");
    $row = $STH->fetch();
-   $STH = $conn->query("SELECT floorLevel, RoomNumber, RoomCapacity, RoomType, FacilitiesAvailable FROM Rooms r, Facilities f, Building b
-   WHERE r.RoomsID = f.RoomsID AND b.BuildingID=r.BuildingID AND b.BuildingName ='".$_POST['GetBuildDetails']."'");
+   $STH = $conn->query("SELECT RoomNumber AS 'Room Number', floorLevel AS 'Floor Level',  (SELECT Count(floorLevel) FROM Rooms r, Building b WHERE r.BuildingID = b.BuildingID AND b.BuildingID = '".$_POST['sendingBuildingID']."') AS 'Total Rooms on Floor', RoomCapacity AS 'Room Capacity', RoomType AS 'Room Type', FacilitiesAvailable AS 'Facilities Available'
+   FROM Building b, Rooms r LEFT JOIN Facilities f ON r.RoomsID = f.RoomsID WHERE b.BuildingID = r.BuildingID AND b.BuildingID = '".$_POST['sendingBuildingID']."'");
 ?>
 <div class="panel-body">
-   <div class="modal fade" id="listOfCampus" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
          <div class="modal-content">
             <div class="modal-header">
-               <h5 class="modal-title" id="exampleModalCenterTitle">Detail of Building </h5>
+               <h5 class="modal-title">Detail of Building </h5>
                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                <span aria-hidden="true">&times;</span>
                </button>
@@ -260,7 +260,209 @@
 </div>
 <script>
    $(function() {
-     $("#listOfCampus").modal();
+     $("#myModal").modal();
+   });
+</script>
+<?php
+   }
+
+   //********** List of Programs given a department **********//
+   if(isset($_POST['listOfProgram'])) {
+     $STH = $conn->query("SELECT DepartmentID, departmentName FROM Department");
+?>
+<div class="panel-body">
+   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+         <div class="modal-content">
+            <div class="modal-header">
+               <h5 class="modal-title">List of Department</h5>
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+               </button>
+            </div>
+            <div class="modal-body">
+               <form  method="post">
+                  <label for="sel1">Choose a Department</label>
+                  <select class="form-control" name="sendingDepartmentID">
+<?php
+                     while($row = $STH->fetch()) {
+                       echo "<option name='display' value='".$row['DepartmentID']."'>".$row['departmentName']."</option>";
+                     }
+?>
+                  </select>
+                  <div class="modal-footer">
+                     <button type="submit" class="btn btn-primary" name="ButtonThree">Submit</button>
+                  </div>
+               </form>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<script>
+   $(function() {
+   $("#myModal").modal();
+   });
+</script>
+<?php
+   }
+
+   if(isset($_POST['ButtonThree'])) {
+   $STH = $conn->query("SELECT p.programName AS 'Program Name', d.departmentName AS 'Department Name', u.underGradCredit AS 'Credits' FROM Program p, Department d, UnderGraduateProgram u WHERE u.ProgramID = p.ProgramID AND p.DepartmentID = d.DepartmentID AND d.DepartmentID = '".$_POST['sendingDepartmentID']."'");
+?>
+<div class="panel-body">
+   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+         <div class="modal-content">
+            <div class="modal-header">
+               <h5 class="modal-title">Program Available</h5>
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+               </button>
+            </div>
+            <div class="modal-body">
+<?php
+                  echo '<table class="table table-bordered  table-striped table-hover text-center">';
+                  $i = 0;
+                  while($row = $STH->fetch(PDO::FETCH_ASSOC)) {
+                      if ($i == 0) {
+                        $i++;
+                        echo "<tr>";
+                        foreach ($row as $key => $value) {
+                          echo "<th>" . $key . "</th>";
+                        }
+                        echo "</tr>";
+                      }
+                      echo "<tr>";
+                      foreach ($row as $value) {
+                        echo "<td>" . $value . "</td>";
+                      }
+                    echo "</tr>";
+                  }
+                  echo "</table>";
+?>
+            </div>
+            <div class="modal-footer">
+               <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<script>
+   $(function() {
+     $("#myModal").modal();
+   });
+</script>
+<?php
+   }
+
+   //********** List of courses given a term and program **********//
+   if(isset($_POST['listCourseTermProg'])) {
+     $STH = $conn->query("SELECT SessionID, session FROM Session");
+     $STHTWO = $conn->query("SELECT DISTINCT year FROM Term");
+     $STHTHREE = $conn->query("SELECT ProgramID, programName FROM Program");
+?>
+<div class="panel-body">
+   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+         <div class="modal-content">
+            <div class="modal-header">
+               <h5 class="modal-title">List of Department</h5>
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+               </button>
+            </div>
+            <div class="modal-body">
+               <form  method="post">
+                  <label for="sel1">Choose a Session</label>
+                  <select class="form-control" name="sendingSessionID">
+<?php
+                     while($row = $STH->fetch()) {
+                       echo "<option name='display' value='".$row['SessionID']."'>".$row['session']."</option>";
+                     }
+?>
+                  </select>
+                  <label for="sel1">Choose a Year</label>
+                  <select class="form-control" name="sendingYear">
+<?php
+                     while($row = $STHTWO->fetch()) {
+                       echo "<option name='display' value='".$row['year']."'>".$row['year']."</option>";
+                     }
+?>
+                  </select>
+                  <label for="sel1">Choose a Program Name</label>
+                  <select class="form-control" name="sendingProgramID">
+<?php
+                     while($row = $STHTHREE->fetch()) {
+                       echo "<option name='display' value='".$row['ProgramID']."'>".$row['programName']."</option>";
+                     }
+?>
+                  </select>
+                  <div class="modal-footer">
+                     <button type="submit" class="btn btn-primary" name="ButtonFour">Submit</button>
+                  </div>
+               </form>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<script>
+   $(function() {
+   $("#myModal").modal();
+   });
+</script>
+<?php
+   }
+
+   if(isset($_POST['ButtonFour'])) {
+   $STH = $conn->query("SELECT c.name AS 'Course Offered'
+FROM Course c, Section s, Term t, Department d, Program p
+WHERE c.CourseID = s.CourseID AND c.DepartmentID = d.DepartmentID AND d.DepartmentID = p.DepartmentID AND s.TermID = t.TermID AND SessionID = '".$_POST['sendingSessionID']."' AND year = '".$_POST['sendingYear']."' AND ProgramID = '".$_POST['sendingProgramID']."'");
+?>
+<div class="panel-body">
+   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+         <div class="modal-content">
+            <div class="modal-header">
+               <h5 class="modal-title">Course Offered</h5>
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+               </button>
+            </div>
+            <div class="modal-body">
+<?php
+                  echo '<table class="table table-bordered  table-striped table-hover text-center">';
+                  $i = 0;
+                  while($row = $STH->fetch(PDO::FETCH_ASSOC)) {
+                      if ($i == 0) {
+                        $i++;
+                        echo "<tr>";
+                        foreach ($row as $key => $value) {
+                          echo "<th>" . $key . "</th>";
+                        }
+                        echo "</tr>";
+                      }
+                      echo "<tr>";
+                      foreach ($row as $value) {
+                        echo "<td>" . $value . "</td>";
+                      }
+                    echo "</tr>";
+                  }
+                  echo "</table>";
+?>
+            </div>
+            <div class="modal-footer">
+               <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<script>
+   $(function() {
+     $("#myModal").modal();
    });
 </script>
 <?php
--- pages/feed.php
+++ pages/feed.php
@@ -9,13 +9,11 @@ require $_SERVER['DOCUMENT_ROOT'] . '/assets/query.php'; //Getting the code from
       <div class="container-fluid content">
         <div class="row">
         <div class="col-md-4 mid"></div>
-
          <div class="col-md-4 mid">
                <h2 class="display-4" align="center" >
-                  <div> PRESS THE BUTTON >:D </div>
+                  <div>PRESS THE BUTTON >:D</div>
                </h2>
             <br>
-
               <form method="post">
                 <a class="btn btn-lg btn-primary btn-block" href="https://crc353.encs.concordia.ca/pages/student.php">Student Modification</a>
                 <a class="btn btn-lg btn-secondary btn-block" href="https://crc353.encs.concordia.ca/pages/faculty.php">Faculty Member Modification</a>
@@ -23,8 +21,9 @@ require $_SERVER['DOCUMENT_ROOT'] . '/assets/query.php'; //Getting the code from
                 <button class="btn btn-lg btn-danger btn-block" type="submit" name="listOfCampus">List of All Campuses</button>
                 <button class="btn btn-lg btn-warning btn-block" type="submit" name="listOfBuildings" >List of Buildings given a campus</button>
                 <button class="btn btn-lg btn-info btn-block" name="DetailListOfBuildings">Building Information given a building</button>
-                <button class="btn btn-lg btn-dark btn-block disabled" name="">List of Programs given a department</button>
-                <button class="btn btn-lg btn-primary btn-block disabled" name="">List of courses given a term and program</button>
+                ^-- Need to fix the query
+                <button class="btn btn-lg btn-dark btn-block" name="listOfProgram">List of Programs given a department</button>
+                <button class="btn btn-lg btn-primary btn-block" name="listCourseTermProg">List of courses given a term and program</button>
                 <button class="btn btn-lg btn-secondary btn-block disabled" name="">List of courses given a term and department</button>
                 <button class="btn btn-lg btn-success btn-block disabled" name="">List of students given a term and program</button>
                 <button class="btn btn-lg btn-danger btn-block disabled" name="">List of instructors given a term and course</button>
@@ -39,15 +38,9 @@ require $_SERVER['DOCUMENT_ROOT'] . '/assets/query.php'; //Getting the code from
                 <button class="btn btn-lg btn-info btn-block disabled" name="">Register student in a course</button>
                 <button class="btn btn-lg btn-dark btn-block disabled" name="">Drop a course for a student</button>
                 <button class="btn btn-lg btn-primary btn-block disabled" name="">Detail report given a student</button>
-
-
               </form>
-
-
          </div>
-
          <div class="col-md-4 mid"></div>
-
       </div>
       </div>
       <?php require $_SERVER['DOCUMENT_ROOT'] . '/repeated/footer.php'; ?> <!-- Getting the code from footer.php file. -->
